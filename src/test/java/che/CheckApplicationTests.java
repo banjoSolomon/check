24ec6e231db6f7ccca968e80e9ec295b3fa37ba9
Solomon <ayomidebanjo02@gmail.com>
2024-10-18 15:18:29 +0100

    first commit

---
+++ src/test/java/che/CheckApplicationTests.java
@@ -0,0 +1,13 @@
+package che;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
